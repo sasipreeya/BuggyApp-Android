e67bc850995cf45b8c39ca4c880628a1e626b8d6
Sasipreeya Sukprasert <sasipreeya.sukprasert@scb.co.th>
2019-08-19 17:21:49 +0700

    profile complete

---
+++ app/src/main/kotlin/scb/academy/jinglebell/activity/WelcomeActivity.kt
@@ -0,0 +1,25 @@
+package scb.academy.jinglebell.activity
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_welcome.*
+import scb.academy.jinglebell.R
+
+
+class WelcomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+
+        val nickname: String = intent.getStringExtra("nickname")
+        name.setText(nickname)
+    }
+
+}
--- app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
+++ app/src/main/kotlin/scb/academy/jinglebell/fragment/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package scb.academy.jinglebell.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -7,33 +8,29 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_profile.*
 import kotlinx.android.synthetic.main.fragment_profile.view.*
 import scb.academy.jinglebell.R
+import scb.academy.jinglebell.activity.WelcomeActivity
 
 class ProfileFragment : Fragment() {
 
+    @Suppress("UNREACHABLE_CODE")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
 
-        nameEditText.addTextChangedListener(object : TextWatcher {
-            override fun afterTextChanged(s: Editable?) {
-                var name: String = nameEditText.text.toString()
-                nameEditText.visibility = View.GONE
-                nameTextView.text = name
+        nameEditText.setOnEditorActionListener({ v, actionId, event ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                val intent = Intent(context, WelcomeActivity::class.java)
+                intent.putExtra("nickname", nameEditText.text.toString())
+                startActivity(intent)
+
+                handled = true
             }
-
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                var name: String = nameEditText.text.toString()
-                nameEditText.visibility = View.GONE
-                nameTextView.text = name
-            }
-
+            handled
         })
     }
 
